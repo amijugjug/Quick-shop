775e4be3e3da74bf5ce15d6a1c8fb76f79b58ead
Prakhar Srivastava <sriprakhar2@gmail.com>
2024-07-10 21:25:11 +0530

    chore: added hook for detecting device

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
 import { ModalProvider } from './context/Modal.context';
+import { useDetectDevice } from './hooks/useDetectDevice.hook';
 import AppRouter from './router';
 
 function App() {
+  const { deviceType, width } = useDetectDevice();
+  const isMobile = deviceType === 'mobile';
+  if (isMobile) {
+    return <div>Website is not accessible via phone</div>;
+  }
   return (
     <ModalProvider>
       <AppRouter />
---
+++ src/hooks/useDetectDevice.hook.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+export const useDetectDevice = () => {
+  const [device, setDevice] = useState(null);
+  const [width, setWidth] = useState(null);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    const userAgent = window.navigator.userAgent;
+    const windowWidth = window.innerWidth;
+
+    if (/Mobi|Android/i.test(userAgent)) {
+      setDevice('mobile');
+    } else if (/iPad|iPhone|iPod/i.test(userAgent)) {
+      setDevice('tablet');
+    } else {
+      setDevice('desktop');
+    }
+
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    };
+  }, []);
+
+  return { device, width };
+};
