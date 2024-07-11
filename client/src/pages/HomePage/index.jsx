0bb78b6aa61087a4df65f6b4e33fa1b2e417469c
amijugjug <sriprakhar2@gmail.com>
2024-07-12 01:27:13 +0530

    fix(eslint): eslint issues resolved

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import styles from './HomePage.module.css';
 import Button from '../../components/atoms/Button';
 import Image from '../../components/atoms/Image';
+import { useUser } from '../../context/User.context';
 import { isUserLoggedIn } from '../../services/auth.service';
 import ContemplativeAthleisure from '../../static/assets/Contemplative Athleisure.jpg';
 import ContemplativeFashionPortrait from '../../static/assets/Contemplative Fashion Portrait.jpg';
 import VibrantAthleticWearCollection from '../../static/assets/Vibrant Athletic Wear Collection.jpg';
-import { useUser } from '../../context/User.context';
-import { useEffect } from 'react';
 
 function HeroSection({ navigate }) {
   const handleNavigation = () => {
