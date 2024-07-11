c19d604b0b44857440df1a879cd14877372cb659
amijugjug <sriprakhar2@gmail.com>
2024-07-11 23:11:31 +0530

    fix(style): fixed cart component

--- src/components/CartPageComponents/CartItem.jsx
+++ src/components/CartPageComponents/CartItem.jsx
@@ -5,22 +5,27 @@ import Image from '../../components/atoms/Image';
 import AddToCartComponent from '../AddToCartComponent';
 import { RemoveFromWishlist } from '../AddToWishlist';
 
-
 const CartItem = ({ item, isWishlist }) => {
   return (
     <div className={styles.cartItem}>
       <div className={styles.imageContainer}>
-        <Image src={item.image} alt={item.title} className={styles.image} />
+        <Image
+          src={item.image}
+          alt={item.title}
+          className={styles.image}
+          width={200}
+          height={200}
+        />
       </div>
       <div className={styles.detailsContainer}>
         <div className={styles.textContainer}>
           <div className={styles.productName}>{item.title}</div>
           <div className={styles.productDescription}>{item.description}</div>
         </div>
-      </div>
-      <div className={styles.actionsContainer}>
-        {isWishlist ? <RemoveFromWishlist product={item} /> : null}
-        <AddToCartComponent product={item} showCount={true} />
+        <div className={styles.actionsContainer}>
+          {isWishlist ? <RemoveFromWishlist product={item} /> : null}
+          <AddToCartComponent product={item} showCount={true} />
+        </div>
       </div>
     </div>
   );
