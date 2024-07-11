6365a1d444b48a4aa60f06e6bdd7520e7c18ad21
amijugjug <sriprakhar2@gmail.com>
2024-07-11 21:16:00 +0530

    fix(not found page): background styling

--- src/components/MobileWarning/index.jsx
+++ src/components/MobileWarning/index.jsx
@@ -1,28 +1,26 @@
+import styles from './MobileWarning.module.scss';
 const MobileWarning = () => {
   return (
-    <div style={styles.container}>
-      <h1 style={styles.message}>
+    <div className={styles.container}>
+      <div className={styles.area}>
+        <ul className={styles.circles}>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <h1 className={styles.message}>
         Website is not available on mobile devices.
       </h1>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    backgroundColor: '#4e54c8',
-    color: '#fff',
-    textAlign: 'center',
-    padding: '20px',
-  },
-  message: {
-    fontSize: '24px',
-    fontWeight: 'bold',
-  },
 };
 
 export default MobileWarning;
