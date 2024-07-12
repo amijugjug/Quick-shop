435b9b5a2e2a7c8bb8548096d65da88dc5c1cf77
Prakhar Srivastava <sriprakhar2@gmail.com>
2024-07-12 10:41:21 +0530

    chore: routes corrected, PORT from env

--- server/app.js
+++ server/app.js
@@ -27,13 +27,13 @@ app.post("/api/create-checkout-session", async (req, res) => {
     payment_method_types: ["card"],
     line_items: lineItems,
     mode: "payment",
-    success_url: "http://localhost:3000/sucess",
-    cancel_url: "http://localhost:3000/cancel",
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/failed",
   });
 
   res.json({ id: session.id });
 });
 
-app.listen(7000, () => {
+app.listen(process.env.PORT, () => {
   console.log("server start");
 });
